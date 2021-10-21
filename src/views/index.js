132ac446c164bde606154d92c1a759afe737a4fa
Benoit-Ferrer <Ferrer.benoit@orange.fr>
2021-10-21 19:56:28 +0200

    d

--- src/views/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-export { default as UnauthorizedView } from './UnauthorizedView'
-export { default as NotFoundView } from './NotFoundView'
-export { default as AboutView } from './AboutView'
-export { default as IndexView } from './IndexView'
-export { default as StyleguideView } from './StyleguideView'
-
-/* eslint-enable */
